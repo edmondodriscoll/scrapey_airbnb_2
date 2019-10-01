e2e2b2b93264e0662b41ad26d1a1269395aca7e8
Improvewell <improvewell@Edmonds-MacBook-Pro-3.local>
2019-10-01 17:14:00 +0100

    update

--- scrapey_update.py
+++ scrapey_update.py
@@ -14,3 +14,24 @@ import datetime
 datetime_object = datetime.datetime.now()
 print(datetime_object)
 
+
+# In[18]:
+
+
+import os 
+
+
+# In[21]:
+
+
+
+os.system('touch today1' )
+
+
+
+
+# In[ ]:
+
+
+
+
