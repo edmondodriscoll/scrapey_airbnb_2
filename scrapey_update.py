d1032557aa5a1ec681cd6f1cba9ee66edf04ced6
Improvewell <improvewell@Edmonds-MacBook-Pro-3.local>
2019-10-01 15:57:34 +0100

    update

---
+++ scrapey_update.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+
+
+# In[2]:
+
+
+os.system("git add .")
+
+
+# In[3]:
+
+
+os.system('git commit -m"update"' )
+
+
+# In[4]:
+
+
+os.system("git push")
+
+
+# In[ ]:
+
+
+
+
