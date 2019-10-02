6abd1c68fdae982608515f5bcd6efa317f68f9b6
Improvewell <improvewell@Edmonds-MacBook-Pro-3.local>
2019-10-02 11:02:29 +0100

    update

--- scrapey_update.py
+++ scrapey_update.py
@@ -21,13 +21,54 @@ print(datetime_object)
 import os 
 
 
-# In[21]:
+# In[22]:
 
 
 
 os.system('touch today1' )
 
 
+
+
+# In[44]:
+
+
+import random
+list =[]
+for x in range(1):
+    y= random.randint(1,3)
+    list.append(y)
+    print(random.randint(1,3))
+
+
+# In[45]:
+
+
+list
+
+
+# In[46]:
+
+
+import pandas as pd
+
+
+# In[47]:
+
+
+df = pd.DataFrame(list, columns = ["test"])
+
+
+# In[48]:
+
+
+df
+
+
+# In[49]:
+
+
+df.to_csv("test.csv")
 
 
 # In[ ]:
